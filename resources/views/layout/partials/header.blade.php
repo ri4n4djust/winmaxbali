0a3386ea8f0b934e3d88db8667d5cc20899a065f
ri4n4djust <rian.adjust@gmail.com>
2021-08-18 15:28:35 +0800

    Update header.blade.php

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -6,7 +6,7 @@
       <h2>Kami merupakan perusahan IT berlokasi di bali. yang mengerjain semua tentang IT.<br> 
       Mohon maaf kita masi dalam pengembangan bisa ada yang ugent bisa klik tombol Whatsapp di bawah</h2>
       <div class="d-flex">
-        <a href="#" class="venobox btn-watch-video" >+62 8186 88 114<i class="icofont-brand-whatsapp"></i></a>
+        <a href="https://wa.me/62818688114" target="_blank" class="btn-watch-video" >+62 8186 88 114<i class="icofont-brand-whatsapp"></i></a>
         <a href="#" class="venobox btn-watch-video" data-vbtype="video" data-autoplay="true"> Watch Video <i class="icofont-play-alt-2"></i></a>
       </div>
     </div>
