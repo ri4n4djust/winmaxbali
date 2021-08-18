9f2afcabc0385cf282c3674e74a4acc796327bcc
ri4n4djust <rian.adjust@gmail.com>
2021-08-18 17:58:53 +0800

    new

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -3,8 +3,8 @@
     <div class="container" data-aos="zoom-out" data-aos-delay="100">
       <h1>Welcome to <span>WinMax Bali</spa>
       </h1>
-      <h2>Kami merupakan perusahan IT berlokasi di bali. yang mengerjain semua tentang IT.<br> 
-      Mohon maaf kita masi dalam pengembangan bisa ada yang ugent bisa klik tombol Whatsapp di bawah</h2>
+      <h2>Kami merupakan perusahan IT berlokasi di bali. yang mengerjakan semua tentang IT.<br> 
+      Mohon maaf kita masi dalam pengembangan, bila ada yang urgent bisa klik tombol Whatsapp di bawah</h2>
       <div class="d-flex">
         <a href="https://wa.me/62818688114" target="_blank" class="btn-watch-video" >+62 8186 88 114<i class="icofont-brand-whatsapp"></i></a>
         <a href="#" class="venobox btn-watch-video" data-vbtype="video" data-autoplay="true"> Watch Video <i class="icofont-play-alt-2"></i></a>
