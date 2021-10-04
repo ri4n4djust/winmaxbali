f2d59bb94e8a0e31c9e45ebe7a606ddefb6e8619
ri4n4djust <rian.adjust@gmail.com>
2021-10-05 07:35:37 +0800

    New

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', [App\Http\Controllers\masterController::class, 'index']);
 //     return view('welcome');
 // });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
