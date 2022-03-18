facd109e3774c987c76b0ba7779a209cdc39663d
ri4n4djust <rian.adjust@gmail.com>
2022-03-18 12:37:03 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/album', [App\Http\Controllers\masterController::class, 'indexAlbum']);
+Route::get('/callback', [App\Http\Controllers\masterController::class, 'callback']);
