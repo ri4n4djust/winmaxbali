bb8185956c902c6efabc14a2ac86289256e605e6
ri4n4djust <rian.adjust@gmail.com>
2022-03-18 12:38:18 +0800

    Update masterController.php

--- app/Http/Controllers/masterController.php
+++ app/Http/Controllers/masterController.php
@@ -40,16 +40,16 @@ class masterController extends Controller
 
     public function callback(Request $request)
     {
-        $data = file_get_contents('https://developer.iak.id/api/sandbox/prepaid/0');
+        // $data = file_get_contents('https://developer.iak.id/api/sandbox/prepaid/0');
         // $my_file = 'file.txt';
         // $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
         // fwrite($handle, $data);
         // fclose($handle);
         // return view('callback', ['hasil' => $data]);
-        return response([$data], 200);
+        // return response([$data], 200);
 
-        // json_decode($request->getContent(), true);
-        // return json_decode($request->getContent(), true);
+        json_decode($request->getContent(), true);
+        return json_decode($request->getContent(), true);
         // $response = Http::post('https://developer.iak.id/api/sandbox/prepaid/success');
         // return response([$response], 200);
     }
