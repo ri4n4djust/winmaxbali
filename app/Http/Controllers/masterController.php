3a542a97f4bac941ea54baad57d5224f4dc75d6a
ri4n4djust <rian.adjust@gmail.com>
2022-03-19 07:15:58 +0800

    Update masterController.php

--- app/Http/Controllers/masterController.php
+++ app/Http/Controllers/masterController.php
@@ -62,8 +62,8 @@ class masterController extends Controller
         // $collection = collect( $key_value);
         // $filtered = $collection->where('product_description', $cariop)->where('product_type', 'pulsa');
         // return $body;
-        $json = file_get_contents('php://input');
-        $obj = json_decode($json, TRUE);
-        echo $obj ;
+        // $json = file_get_contents('php://input');
+        // $obj = json_decode($json, TRUE);
+        // echo $obj ;
     }
 }
