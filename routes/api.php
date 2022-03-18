f25a3433d0256f580861f7a0228f2aa3074a3dba
ri4n4djust <rian.adjust@gmail.com>
2022-03-18 20:42:53 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/album', [App\Http\Controllers\masterController::class, 'indexAlbum']);
-Route::get('/callback', [App\Http\Controllers\masterController::class, 'callback']);
+Route::post('/callback', [App\Http\Controllers\masterController::class, 'callback']);
