89a647e15c791cc27e9d1802bd0471e002322bed
ri4n4djust <rian.adjust@gmail.com>
2022-03-19 10:50:16 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::post('/callback', function (Request $request) {
     // $body =json_decode($body); 
     // $ref_id = $body->data->ref_id; //access key
     DB::table('pulsa')->insert([
-        // 'ref_id' => $path,
+        'ref_id' => $path,
         // 'price' => $body->data->price,
         // 'message' => $body->data->message,
         // 'status' => $body->data->status
