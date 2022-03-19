3469dc5d2e4900fa2f7d5f49694c48fca9b83ec4
ri4n4djust <rian.adjust@gmail.com>
2022-03-19 13:07:15 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,6 @@ Route::post('/callback', function (Request $request) {
         'status' => $body->data->status
     ]);
     // echo $requests;
-    // Storage::disk('local')->put('pulsa', $body1);
-    Session::put('public', $body1);
+    Storage::disk('public')->put('pulsa', $body1);
+    // Session::put('pulsa', $body1);
 }); 
