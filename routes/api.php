e43eeb656f5f25ac5a777b2060084781f9a17835
ri4n4djust <rian.adjust@gmail.com>
2022-03-19 10:51:36 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,11 +22,11 @@ Route::get('/album', [App\Http\Controllers\masterController::class, 'indexAlbum'
 // Route::post('/callback', [App\Http\Controllers\masterController::class, 'callback']);
 Route::post('/callback', function (Request $request) {
     $body = $request->getContent();
-    $path = $request->path();
-    // $body =json_decode($body); 
+    // $path = $request->path();
+    $body =json_decode($body); 
     // $ref_id = $body->data->ref_id; //access key
     DB::table('pulsa')->insert([
-        'ref_id' => $path,
+        'ref_id' => $body->data->price,
         // 'price' => $body->data->price,
         // 'message' => $body->data->message,
         // 'status' => $body->data->status
