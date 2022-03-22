bf8ab10f93bfc524c6da80e0b70abb864e80ffd9
ri4n4djust <rian.adjust@gmail.com>
2022-03-22 20:17:04 +0800

    new

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,7 @@ Route::post('/send-notification', [HomeController::class, 'sendNotification'])->
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
