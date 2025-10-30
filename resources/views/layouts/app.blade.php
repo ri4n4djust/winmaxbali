05c2859e0a90086e2e1ed890645cad3a5018df65
ri4n4djust <rian.adjust@gmail.com>
2025-10-30 12:48:30 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src 'self' http://*.cke-cs.com; script-src 'self'; img-src * data:; style-src 'self' 'unsafe-inline'; frame-src *">
 
         <title>Admin - Dashboard</title>
 
